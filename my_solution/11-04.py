b4f38b9613c9bfad53fde1bcb1ea5f58f1f5d8ab
leeht <leeht0113@gmail.com>
2023-08-29 00:14:43 +0900

    v13

---
+++ my_solution/11-04.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+coin = list(map(int, input().split()))
+coin.sort()
+
+target = 1
+for c in coin:
+    if target < c:
+        break
+    target += c
+print(target)
