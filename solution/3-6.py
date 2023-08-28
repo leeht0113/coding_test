cc4197da98b7589a1b46a97ffb845b63a193f1c8
leeht <leeht0113@gmail.com>
2023-08-28 22:59:41 +0900

    v12

---
+++ solution/3-6.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    # (N==K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
+    target = (n//k)*k
+    result += (n-target) # 완전히 나누어 떨어지면 0이 더해짐
+    n = target # k로 나누어떨어질 수로 변함
+    # N이 K보다 작을 떄(더 이상 나눌 수 없을 때)반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    result += 1
+    n //= k
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n-1)
+print(result)
